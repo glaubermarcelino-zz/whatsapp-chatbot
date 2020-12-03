94bf30cafbe50a82a25a7c4bb28fda4c8e3088ce
Glauber M. da Silva <gmsoft.informatica@gmail.com>
2020-12-03 13:56:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ venom
 function start(client) {
   client.onMessage((message) => {
     // if (message.body === 'Oi' || message.body === 'Olá') {
-      client.sendText(message.from, `Olá! Tudo bem com você ${message.chat.contact.name}? \n No momento estou indisponível , mas pode deixar sua mensagem que logo entrarei em contato`)
+      if(!message.body.isGroup)
+      {
+      client.sendText(message.from, `Olá! Tudo bem com você ${message.chat.contact.pushname}? \n No momento estou indisponível , mas pode deixar sua mensagem que logo entrarei em contato`)
         .then((result) => console.log('Result: ', result))
         .catch((erro) => console.log('Erro ao enviar a mensagem %s', erro));
-    // }
+      }
   })
 }
