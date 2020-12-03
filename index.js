d8888d43b04376d75bc4360d7419fde4ef7fbea1
Glauber Marcelino <gmsoft.informatica@gmail.com>
2020-12-03 07:23:50 -0300

    Adicionado mensagem com o nome do usuário e package venom-bot

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ venom
 
 function start(client) {
   client.onMessage((message) => {
-    if (message.body === 'Oi' || message.body === 'Olá') {
-      client.sendText(message.from, `Olá! Tudo bem com você ${message.from}?`)
+    // if (message.body === 'Oi' || message.body === 'Olá') {
+      client.sendText(message.from, `Olá! Tudo bem com você ${message.chat.contact.name}? \n No momento estou indisponível , mas pode deixar sua mensagem que logo entrarei em contato`)
         .then((result) => console.log('Result: ', result))
         .catch((erro) => console.log('Erro ao enviar a mensagem %s', erro));
-    }
+    // }
   })
 }
